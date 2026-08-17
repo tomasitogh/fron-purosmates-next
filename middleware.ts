716781f9937208eef1aedb3bf52ba28b8f966f8b
Tomas Gonzalez Humphreys <tomasgonzalezhu.phreys12@gmail.com>
2026-08-17 11:13:19 -0300

    SEO fix: exclude clerk middleware from /nosotros, /shop and more.

--- middleware.ts
+++ middleware.ts
@@ -4,14 +4,10 @@ const isAdminRoute = createRouteMatcher(['/admin(.*)']);
 
 export default clerkMiddleware(async (auth, req) => {
   if (isAdminRoute(req)) {
-    // Proteger ruta - requiere estar autenticado
     await auth.protect();
   }
 });
 
 export const config = {
-  matcher: [
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    '/(api|trpc)(.*)',
-  ],
+  matcher: ['/admin(.*)', '/(api|trpc)(.*)'],
 };
